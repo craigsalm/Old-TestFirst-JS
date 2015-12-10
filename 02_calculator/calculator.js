da246b8626ba1ca886972c2037729d09fe21d115
Craig Salm <craig.salm@gmail.com>
2015-12-09 22:11:10 -0500

    Second attempt to fix calculator, this time creating a variable object rather than function like the previous two examples.

--- 02_calculator/calculator.js
+++ 02_calculator/calculator.js
@@ -1,14 +1,19 @@
 calculator.js
-function createCalculator() {
-	var value = 0;
+var createCalculator = function() {
 	var calculator = {};
-	calculator.add = function (number) {
-		return value + number;
-	}
-	calculator.subtract = function (number) {
-		return value - number;
-	}
+	calculator.total = 0;
 
+	calculator.value = function() {
+		return this.total;
+	};
+	calculator.add = function(number) {
+		this.total = this.total + number:
+	};
+	calculator.subtract = function(number) {
+		this.total = this.total - number:
+	};
+	return calculator;
 }
+var calculator = createCalculator;
 /**None of these tests are working, must be something wrong with
 initial set-up of the calculator object that I can't figure out.**/
